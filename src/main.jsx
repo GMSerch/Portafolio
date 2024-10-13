fc25fffd3f909f06e0f3c8c690ef9df6f46202da
Sergio Luis Flores Q. <Sergioluisf6@gmail.com>
2024-10-13 11:45:16 -0600

    Inicializacion de Pagina Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import HomePage from './pages/HomePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage></HomePage>}  />
+    </Routes>
+   </BrowserRouter>
 )
